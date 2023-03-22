46fe11c48ed4c1a70cd065016dd656bfc00eefe2
ILoveRicardo <siebe.wiersma@sintlucasedu.nl>
2023-03-22 16:37:23 +0100

    incocnito window

--- java/index.js
+++ java/index.js
@@ -2,14 +2,27 @@ document.getElementById("mainTitle").innerText = "Point and click adventure";
 
 const gameWindow = document.getElementById("gameWindow");
 const mainCharacter = document.getElementById("mainCharacter");
+const offsetCharacter = 16;
 
 gameWindow.onclick = function(e){
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
 
-    mainCharacter.style.left = x + "px";
-    mainCharacter.style.top = y + "px";
+    mainCharacter.style.left = x - offsetCharacter + "px";
+    mainCharacter.style.top = y - offsetCharacter + "px";
 
-    console.log(x + "-" + y);
+    console.log(e.target.id);
+
+    switch(e.target.id){
+        case "door":
+            console.log("door clicked")
+            break
+        case "fire":
+            console.log("fire clicked")
+            break
+        default:
+            console.log("Me when the when the me the me when")
+            break
+    }
 }
