f5c0b0a4d4b3a44bc614b005f401f5523db69d2a
ILoveRicardo <siebe.wiersma@sintlucasedu.nl>
2023-04-14 12:41:57 +0200

    qwertyuiop111111

--- java/index.js
+++ java/index.js
@@ -1,28 +1,58 @@
-document.getElementById("mainTitle").innerText = "Point and click adventure";
-
+document.getElementById("mainTitle").innerText = "Point and Click adventure";
+const offsetCharacter = 16;
+const sec = 1000;
 const gameWindow = document.getElementById("gameWindow");
 const mainCharacter = document.getElementById("mainCharacter");
-const offsetCharacter = 16;
+const mainCharacterSpeech = document.getElementById("mainCharacterSpeech");
 
-gameWindow.onclick = function(e){
-    var rect = gameWindow.getBoundingClientRect();
-    var x = e.clientX - rect.left;
-    var y = e.clientY - rect.top;
+const counterSpeech = document.getElementById("counterSpeech");
+const counterCharacter = document.getElementById("counterCharacter");
 
-    mainCharacter.style.left = x - offsetCharacter + "px";
-    mainCharacter.style.top = y - offsetCharacter + "px";
+const characterAudio = document.getElementById("characterAudio");
+const counterAudio = document.getElementById("counterAudio");
 
-    console.log(e.target.id);
+gameWindow.onclick = function (e) {
+    if (mainCharacterSpeech.style.opacity == 0) {
+        var rect = gameWindow.getBoundingClientRect();
+        var x = e.clientX - rect.left; //x position within the element.
+        var y = e.clientY - rect.top;  //y position within the element.
 
-    switch(e.target.id){
-        case "door":
-            console.log("door clicked")
-            break
-        case "fire":
-            console.log("fire clicked")
-            break
-        default:
-            console.log("Me when the when the me the me when")
-            break
+        mainCharacter.style.left = x - offsetCharacter + "px";
+        mainCharacter.style.top = y - offsetCharacter + "px";
+
+        console.log(e.target.id);
+        switch (e.target.id) {
+            case "closet":
+                showMessage(mainCharacterSpeech, characterAudio, "where is my hoodie?");
+                break;
+            case "fire":
+                showMessage(mainCharacterSpeech, characterAudio, "Why did i put my hand in the fire...?");
+                break;
+            case "otherCharacter":
+                showMessage(mainCharacterSpeech, characterAudio, "damn this guy is ugly");
+                setTimeout(function () { counterCharacter.style.opacity = 1; }, 4 * sec);
+                setTimeout(showMessage, 4 * sec, counterSpeech, counterAudio, "we look the same dumbass");
+                setTimeout(showMessage, 8 * sec, mainCharacterSpeech, characterAudio, "no i have a bigger.... nevermind");
+                setTimeout(function () { counterCharacter.style.opacity = 0; }, 12 * sec);
+                break;
+            default:
+                hideMessage(mainCharacterSpeech, characterAudio);
+                hideMessage(counterSpeech, counterAudio);
+                break;
+        }
     }
+}
+
+function showMessage(targetBubble, targetAudio, message) {
+    targetAudio.currentTime = 0;
+    targetAudio.play();
+    targetBubble.innerHTML = message;
+    targetBubble.style.opacity = 1;
+    setTimeout(hideMessage, 4 * sec, targetBubble, targetAudio);
+}
+
+function hideMessage(targetBubble, targetAudio) {
+    targetAudio.pause();
+    targetBubble.innerHTML = "...";
+    targetBubble.style.opacity = 0;
 }
